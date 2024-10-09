57bc3fd2528956615b1ef09d8062c20d9ea0e9cb
benStella <benstella13@gmail.com>
2024-10-08 23:04:18 -0400

    completed timer

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-var timer = $('#timer');
+let timerElem = $('#timer');
 
 function Timer() {
-    var seconds = 55;
+    let seconds = 55;
+    let minutes = 59; 
+    let hours = 0;
 
-        let secInterval = setInterval(function() {
-            if(seconds < 60) {
+    let secInterval = setInterval(function() {
+        if(seconds < 60) {
 
-                timer.text(seconds);
-                console.log(seconds);
-                seconds++;
-            }
-            else if(seconds >= 60) {
-                    clearInterval(secInterval);
-                    seconds=0;
-            }
-        }, 1000);
-
-
-    //     // setInterval(function() {
-    //     //     timer.text(seconds);
-    //     //     console.log(seconds);
-    //     //     seconds++;
-    //     // }, 1000);
-    //     // clearInterval();
-    //     console.log(seconds);
-    //     seconds++;
-    // }
-    // else if(seconds > 60) { 
-    //     clearInterval();
-    //     seconds=0;
-    // }
+            timerElem.text(hours + ':' + minutes + '.' + seconds);
+            console.log(hours + ':' + minutes + '.' + seconds);
+            seconds++;
+        }
+        
+        if(seconds == 60) {
+                seconds=0;
+                minutes++;
+        }
+        
+        if(minutes == 60) {
+                minutes=0;
+                hours++;
+        }
+    }, 1000); 
 }
 $('#start-btn').click(Timer);
